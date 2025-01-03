0c92f6c6216ec88b24c93bf5ebe0b5f1b146b87a
EtienneCClarke <etiennecclarke@icloud.com>
2025-01-03 00:54:22 +0000

    Chore: Refactored name for LinkType

--- components/navigation/index.tsx
+++ components/navigation/index.tsx
@@ -1,23 +1,26 @@
-'use client'
+"use client"
 
 import Image from "next/image"
-import { Link, Links } from "./nav-data"
+import { Links, LinkType } from "./nav-data"
 import css from "./navigation.module.css";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { usePathname } from "next/navigation";
 
 const Navigation = () => {
+
+    const pathname = usePathname();
     
     return (
         <nav className={css.navigation__wrapper}>
             <div className={css.navigation__container}>
                 <a href="/" className={css.logo__container}><Image src="/brand-assets/logo.jpg" alt="logo" width={30} height={30} /></a>
-                {Links.map((link: Link, index: number) => {
+                {Links.map((link: LinkType, index: number) => {
                     return (
                         <span className={css.link__container} key={index}>
                             <a
                                 href={link.disabled ? undefined : link.url}
                                 data-disabled={!!link.disabled}
-                                data-active={window.location.pathname.includes(link.url)}
+                                data-active={pathname.includes(link.url)}
                                 className={css.link}
                                 target={link?.target}
                             >{link.label}</a>
