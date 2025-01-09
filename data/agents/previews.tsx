331aaf15007eac5193c2cb60766d5f51784e930a
EtienneCClarke <etiennecclarke@icloud.com>
2025-01-09 16:16:42 +0000

    Chore: Refactor

--- data/agents/previews.tsx
+++ data/agents/previews.tsx
@@ -2,8 +2,8 @@ export const previews: AgentPreviewData[] = [
     {
         id: 'eb76ae17-b9eb-476d-b272-4bde2d85c808',
         image: '/agents/kinkong.jpg',
-        models: ['GPT-4o Mini', 'Claude Haiku'],
-        name: 'KinKong',
+        models: ['KinOS'],
+        name: 'Kin Kong',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed arcu diam. Mauris accumsan augue vitae mi condimentum suscipit'
     },
     {
