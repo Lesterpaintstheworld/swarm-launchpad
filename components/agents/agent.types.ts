70656a250b394ec2086988e77029d210df447c17
EtienneCClarke <etiennecclarke@icloud.com>
2025-01-09 16:17:05 +0000

    Added KinOS to AgentModel type

--- components/agents/agent.types.ts
+++ components/agents/agent.types.ts
@@ -1,4 +1,4 @@
-type AgentModel = 'GPT-4o Mini' | 'Claude Haiku';
+type AgentModel = 'GPT-4o Mini' | 'Claude Haiku' | 'KinOS';
 
 interface AgentPreviewData {
     image: string;
@@ -6,4 +6,9 @@ interface AgentPreviewData {
     description: string; // Max length of 125 characters
     models: AgentModel[];
     id: string; // UUID
+}
+
+interface AgentInfo extends AgentPreviewData {
+    gallery: React.ReactNode[] | string[];
+    programAddress: string;
 }
