a872b042934f785497d8a53160a418f1270cc0e8
EtienneCClarke <etiennecclarke@icloud.com>
2025-01-11 17:56:34 +0000

    Feat: Started adding portfolio route.

---
+++ app/invest/portfolio/page.tsx
@@ -0,0 +1,12 @@
+export default function Portfolio() {
+
+    return (
+        <main className="container">
+            <div className="h-80 flex flex-col items-center justify-center gap-1">
+                <h2 className="text-center">Your Portfolio</h2>
+                <p className="text-center text-balance text-muted text-lg">Manage your AI swarm investments, and track returns</p>
+            </div>
+        </main>
+    )
+
+}
