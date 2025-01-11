2c6f280e528842cfa30098f8b61a81fec5dac00c
EtienneCClarke <etiennecclarke@icloud.com>
2025-01-11 17:08:45 +0000

    Added `ManagePortfolioCard` to agent page

--- app/invest/[slug]/page.tsx
+++ app/invest/[slug]/page.tsx
@@ -7,6 +7,12 @@ import { Expandable } from "@/components/ui/expandable";
 import { InfiniteCarousel, Slides } from "@/components/ui/infiniteCarousel";
 import { AgentInvestCard } from "@/components/agents/invest";
 import { AgentRecentMarketListings } from "@/components/agents/recentMarketListings";
+import { Card } from "@/components/ui/card";
+import { Button } from "@/components/shadcn/button";
+import Link from "next/link";
+import { LucidePieChart, LucideStore } from "lucide-react";
+import { ConnectButton } from "@/components/solana/connectButton";
+import { ManagePortfolioCard } from "@/components/cards/managePortfolio";
 
 export default function Agent({ params }: { params: { slug: string } }) {
 
@@ -116,6 +122,7 @@ export default function Agent({ params }: { params: { slug: string } }) {
                     },
                 ]}
             />
+            <ManagePortfolioCard />
         </main>
     )
 
