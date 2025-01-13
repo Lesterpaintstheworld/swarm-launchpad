2b7ee32372e09f3c6278dc488f2a22b933f157c3
EtienneCClarke <etiennecclarke@icloud.com>
2025-01-13 16:37:32 +0000

    Added bar chart component

---
+++ components/charts/bar/section.tsx
@@ -0,0 +1,40 @@
+import { cn } from "@/lib/utils";
+import { BarChartItem } from "."
+import { CSSProperties } from "react";
+import React from "react";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/shadcn/tooltip";
+
+interface BarItemProps {
+    data: BarChartItem;
+    className?: string;
+    colour?: string;
+    style?: CSSProperties | undefined;
+}
+
+const BarItem = ({ data, className, colour, style }: BarItemProps) => {
+
+    return (
+        <TooltipProvider>
+            <Tooltip>
+                <TooltipTrigger asChild>
+                    <div
+                        className={cn(`
+                                bg-grey-200 flex-grow ring-none min-h-8 hover:z-10 hover:ring hover:ring-foreground hover:rounded-[4px] transition-border duration-100 last:rounded-r-sm first:rounded-l-sm cursor-pointer`,
+                            className,
+                            colour
+                        )}
+                        style={style}
+                    >
+                    </div>
+                </TooltipTrigger>
+                <TooltipContent className="mb-2 border border-border">
+                    {data?.toolTipContent || data?.label}
+                </TooltipContent>
+            </Tooltip>
+        </TooltipProvider>
+    )
+
+}
+
+export { BarItem };
+export type { BarItemProps };
