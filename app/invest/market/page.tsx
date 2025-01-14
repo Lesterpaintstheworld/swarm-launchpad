071e4ac446032bd113b3d1cbbff97826cd795ca0
EtienneCClarke <etiennecclarke@icloud.com>
2025-01-14 23:38:29 +0000

    Feat: Added sell position card and modal

---
+++ app/invest/market/page.tsx
@@ -0,0 +1,11 @@
+import { SellPositionCard } from "@/components/cards/sellPosition";
+
+export default function Market() {
+
+    return (
+        <main className="container view">
+            <SellPositionCard className="" />
+        </main>
+    )
+
+}
