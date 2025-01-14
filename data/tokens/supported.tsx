32450adb7052cb95604a019adfb179ed2632910a
EtienneCClarke <etiennecclarke@icloud.com>
2025-01-14 23:30:13 +0000

    Added supported tokens top data

---
+++ data/tokens/supported.tsx
@@ -0,0 +1,20 @@
+export const supportedTokens: Token[] = [
+    {
+        label: '$UBC',
+    },
+    {
+        label: '$COMPUTE',
+    },
+    {
+        label: 'SOL',
+        icon: '/tokens/sol.svg',
+    },
+    {
+        label: 'USDC',
+        icon: '/tokens/usdc.svg',
+    },
+    {
+        label: 'USDT',
+        icon: '/tokens/usdt.svg',
+    },
+];
