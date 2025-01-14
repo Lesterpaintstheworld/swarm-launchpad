d45adc9133bf29dbf88ea2f0230b507c06ceef92
EtienneCClarke <etiennecclarke@icloud.com>
2025-01-14 03:17:49 +0000

    Feat: Added `formatSignature` and `sleep` utility functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -19,3 +19,10 @@ export function formatPublicKey(publicKey: string): string {
 	return publicKey?.slice(0, 5) + '...' + publicKey?.slice(-4);
 }
 
+export function formatSignature(signature: string): string {
+	return signature?.slice(0, 12) + '...' + signature?.slice(-3);
+}
+
+export async function sleep(ms: number) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
