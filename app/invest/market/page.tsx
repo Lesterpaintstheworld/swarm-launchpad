ff6b743c47a8b0d9fc4c10e84b9b0f131759b4bf
EtienneCClarke <etiennecclarke@icloud.com>
2025-01-15 01:59:43 +0000

    Feat: Added market page

--- app/invest/market/page.tsx
+++ app/invest/market/page.tsx
@@ -1,10 +1,12 @@
 import { SellPositionCard } from "@/components/cards/sellPosition";
+import { MarketListings } from "@/components/market/listings";
 
 export default function Market() {
 
     return (
         <main className="container view">
-            <SellPositionCard className="" />
+            <SellPositionCard className="mb-6" />
+            <MarketListings />
         </main>
     )
 
