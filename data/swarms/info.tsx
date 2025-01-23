3e5f59e9bc1cd51f9b39c9b0e2a34698c091206b
EtienneCClarke <ecellierclarke@gmail.com>
2025-01-23 12:53:05 -0500

    s

--- data/swarms/info.tsx
+++ data/swarms/info.tsx
@@ -65,7 +65,7 @@ Join the revolution in AI-powered content creation with SLOP FATHER - where ever
     },
     {
         id: 'digitalkin-partner-id',
-        pool: '7ddi6VPLkwmUudc6ay7jQugcfQcP9g9VjcUErJLKcJnP',
+        pool: '',
         image: '/swarms/digitalkin.png',
         models: ['KinOS', 'Claude Haiku'],
         name: 'DigitalKin',
