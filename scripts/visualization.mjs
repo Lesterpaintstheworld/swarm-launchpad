70ac1a8ddab07590bce96c0356b4de1a30000375
Lesterpaintstheworld <reynolds.nicorr@gmail.com>
2025-02-03 03:54:47 +0100

    pushing

--- scripts/visualization.mjs
+++
@@ -1,5 +0,0 @@
-export async function generateMarketCapVisualization(swarmMetrics) {
-    // Log the data instead of generating visualization
-    console.log('Market Cap Data:', swarmMetrics);
-    return;
-}
