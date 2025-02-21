c80274c11ed4e66675a25d9d818cf9dfb2a31f05
EtienneCClarke <etiennecclarke@icloud.com>
2025-02-21 07:22:03 +0000

    Fetch listings for specific pool from `useLaunchpadProgramAccount`. Adjusted props to take a poolid instead of `swarmId`, `numberOfListings`, and `listings`.

--- components/market/recentListings/index.tsx
+++ components/market/recentListings/index.tsx
@@ -5,30 +5,32 @@ import { columns } from "./columns";
 import { Button } from "@/components/shadcn/button";
 import Link from "next/link";
 import { LucideArrowRight } from "lucide-react";
-import { MarketListing } from "../market.types";
+import { useLaunchpadProgramAccount } from "@/hooks/useLaunchpadProgram";
+import { MarketListing } from "@/types/listing";
 
 interface SwarmRecentMarketListingsProps {
-    swarmId: string;
-    listings?: MarketListing[];
-    numberOfListings: number;
+    pool: string;
     className?: string;
 }
 
-const SwarmRecentMarketListings = ({ swarmId, listings, numberOfListings, className }: SwarmRecentMarketListingsProps) => {
+const SwarmRecentMarketListings = ({ pool, className }: SwarmRecentMarketListingsProps) => {
+
+    const { poolListings } = useLaunchpadProgramAccount({ poolAddress: pool });
+    const { data: listings, isFetching } = poolListings;
 
     return (
         <Card className={cn("w-full flex flex-col mt-6 md:mt-12", className)}>
             <h4 className="mb-4 font-foreground font-semibold">P2P Market Listings</h4>
             <DataTable
                 columns={columns}
-                data={listings?.slice(0, numberOfListings) || []}
+                data={isFetching ? [] : listings as MarketListing[]}
             />
             {listings && listings?.length > 0 &&
                 <>
                     <hr />
                     <Button variant='link' className="ml-auto hover:no-underline" asChild>
                         <div className="flex flex-row items-center gap-2 w-fit text-muted mt-4 hover:text-foreground">
-                            <Link href={`/invest/market?swarmId=${swarmId}`}>
+                            <Link href={`/marketplace?tab=listings`}>
                                 View all
                             </Link>
                             <LucideArrowRight />
