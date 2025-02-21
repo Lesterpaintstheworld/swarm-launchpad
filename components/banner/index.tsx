143ed6e78ce02d6a22b9f164eebae3fc11c19439
EtienneCClarke <etiennecclarke@icloud.com>
2025-02-21 09:54:59 +0000

    Updated banner to advertise secondary market.

--- components/banner/index.tsx
+++ components/banner/index.tsx
@@ -5,7 +5,7 @@ const Banner = () => {
     return (
         <div className="w-[100vw] h-fit flex flex-row metallic-bg-ubc py-2 text-sm">
             <div className="container mx-auto flex flex-row justify-between items-center gap-6 flex-wrap">
-                <p className="text-background font-semibold">The launchpad is now live! Invest your <code className="text-sm font-inter px-1">$COMPUTE</code> in agents of tomorrow.</p>
+                <p className="text-background font-semibold">Secondary <Link href={'/marketplace?tab=listings'} className="underline">market</Link> is now live! Buy and sell shares through P2P trading.</p>
                 <Link href="how-it-works" className="text-background text-underline flex flex-row gap-2 hover:bg-foreground/10 px-2 py-1 rounded-[4px] items-center">How does it work<LucideCircleHelp className="w-4" /></Link>
             </div>
         </div>
