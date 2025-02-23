bc7a7852693847952450baa7e84860412f9d5992
EtienneCClarke <etiennecclarke@icloud.com>
2025-02-23 18:45:48 +0000

    Added import for token hooks.

---
+++ hooks/useTokenPrice/index.ts
@@ -0,0 +1,17 @@
+import { useDexScreenerPrice } from "./useDexScreenerPrice";
+import { useUBCPrice } from "./useUBCPrice";
+import { useComputePrice } from "./useComputePrice";
+import { useUSDCPrice } from "./useUSDCPrice";
+import { useUSDTPrice } from "./useUSDTPrice";
+import { useSolPrice } from "./useSolPrice";
+import { useTokenPrice } from "./useTokenPrice";
+
+export { 
+    useDexScreenerPrice,
+    useUBCPrice,
+    useComputePrice,
+    useUSDCPrice,
+    useUSDTPrice,
+    useSolPrice,
+    useTokenPrice
+}
